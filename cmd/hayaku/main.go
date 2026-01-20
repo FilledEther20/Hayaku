94dc9906c457d252501cf541c868d3da8f786ebc
Chaitanya Gairola <gairolachaitanya@gmail.com>
2026-01-20 22:29:13 +0530

    main.go added

--- cmd/hayaku/main.go
+++ cmd/hayaku/main.go
@@ -1,7 +1,24 @@
-package hayaku
+package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+	"github.com/FilledEther20/Hayaku/internal/ratelimiter"
+)
 
 func main() {
-	fmt.Print("Folder structure defined")
+	bucket := ratelimiter.NewTokenBucket(5, 2)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	bucket.Start(ctx)
+
+	for i := 0; i < 10; i++ {
+		startAt := time.Now()
+		time.Sleep(1000 * time.Millisecond)
+		bucket.Wait(context.Background())
+		processedAt := time.Now()
+		fmt.Printf("Processing request[%v] at [%v] processed at [%v]\n", i+1, startAt, processedAt)
+	}
 }
