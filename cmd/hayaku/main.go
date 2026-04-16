8434fb1fe553b9b0a3ad15151c617fe0ec6a7d6a
Chaitanya Gairola <gairolachaitanya@gmail.com>
2026-04-16 23:48:17 +0530

    feat: Sliding window logging based rate limter & Revamp beginning

--- cmd/hayaku/main.go
+++ cmd/hayaku/main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"time"
+
 	"github.com/FilledEther20/Hayaku/internal/ratelimiter"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
-	bucket := ratelimiter.NewTokenBucket(5, 2)
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	bucket.Start(ctx)
+	limiter := ratelimiter.NewSlidingWindowRedis(rdb, 2*time.Second, 5)
 
-	for i := 0; i < 10; i++ {
-		startAt := time.Now()
-		time.Sleep(1000 * time.Millisecond)
-		bucket.Wait(context.Background())
-		processedAt := time.Now()
-		fmt.Printf("Processing request[%v] at [%v] processed at [%v]\n", i+1, startAt, processedAt)
+	// bucket := ratelimiter.NewTokenBucket(5, 2)
+
+	// ctx, cancel := context.WithCancel(context.Background())
+	// defer cancel()
+	// bucket.Start(ctx)
+
+	for i := 0; i < 7; i++ {
+		if limiter.Allow("user_1") {
+			fmt.Printf("Request %d: Allowed\n", i+1)
+		} else {
+			fmt.Printf("Request %d: Denied\n", i+1)
+		}
 	}
+
+	// for i := 0; i < 10; i++ {
+	// 	startAt := time.Now()
+	// 	time.Sleep(1000 * time.Millisecond)
+	// 	bucket.Wait(context.Background())
+	// 	processedAt := time.Now()
+	// 	fmt.Printf("Processing request[%v] at [%v] processed at [%v]\n", i+1, startAt, processedAt)
+	// }
 }
